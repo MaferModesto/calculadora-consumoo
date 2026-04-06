3baca24793af5245da1c9cfff85c6b738c2758c5
Your Name <you@example.com>
2026-04-06 10:21:13 -0300

    Adicionando nome do aplicativo

--- app/src/main/java/com/example/np1mobileproject/MainActivity.java
+++ app/src/main/java/com/example/np1mobileproject/MainActivity.java
@@ -13,51 +13,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Declarando as variáveis, colocando nome nos componentes
     EditText km, litros;
     Button calcular;
     TextView resultado;
 
-    //Código que já vem e a gnt apaga aql parte de erro
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-    //Fazendo a conexão entre os componentes do XML e o JAVA, por meio do id
         km = findViewById(R.id.editKm);
         litros = findViewById(R.id.editLitros);
         calcular = findViewById(R.id.btnCalcular);
         resultado = findViewById(R.id.txtResultado);
 
-        //método quando o usuário clica no botão
         calcular.setOnClickListener( v -> {
 
-            //recebe os valores digitados e converte para String
             String KmRodados = km.getText().toString();
             String litrosAbastecidos = litros.getText().toString();
 
-            //Se KmRodados estiver vazio ou litrosAbastecidos estiver vazio, retorna a msg de erro
             if (KmRodados.isEmpty() || litrosAbastecidos.isEmpty()) {
                 resultado.setText("Preencha todos os campos!");
                 return;
             }
 
-            //convertendo String para números (double)
             double KmNumero = Double.parseDouble(KmRodados);
             double litrosNumero = Double.parseDouble(litrosAbastecidos);
 
-            //Se o EditText (litros) estiver com 0 ou número negativo, retorna a msg de erro
             if (litrosNumero <= 0) {
                 resultado.setText("Erro: Litros não pode ser igual a 0!");
                 return;
             }
 
-            //fazendo a conta
             double consumo = KmNumero / litrosNumero;
 
-            //exibindo o resultado
             resultado.setText("Consumo: " + consumo + " km/L");
 
         });
